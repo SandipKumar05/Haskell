761b8ed758357a4c0e9b87c444be38b4da4094c5
sandipgupta <sandipgupta05@gmail.com>
2015-09-30 23:12:25 +0530

    move without rule

--- board.hs
+++ board.hs
@@ -1,3 +1,5 @@
+import Data.Foldable 
+import Data.Sequence
 
 type Board = [[Maybe Piece]]
 
@@ -19,6 +21,7 @@ instance Show Piece where
 	show Piece{color=Black,player=Rook} = "BR"
 	show Piece{color=Black,player=Knight} = "BN"
 	show Piece{color=Black,player=Pawn} = "BP"
+	show _ = ""
 
 wk = Piece{color=White,player=King}
 wq = Piece{color=White,player=Queen}
@@ -44,4 +47,46 @@ initialBoard = [[Just wr,Just wn,Just wb,Just wq,Just wk,Just wb,Just wn, Just w
 				[Just br,Just bn,Just bb,Just bq,Just bk,Just bb,Just bn, Just br]]
 
 
+change::Int -> Int -> Int -> Int -> Board -> Board
+change x1 y1 x2 y2 a = do 
+		let p = (a!!x1)!!y1
+		let b2 | x1 == x2  = toList $ update y1 Nothing $ fromList (toList $ update y2 p $ fromList (a!!x2))
+			   | otherwise = toList $ update y2 p $ fromList (a!!x2)
+		let b1 | x1 ==x2   = toList $ update y1 Nothing $ fromList (toList $ update y2 p $ fromList (a!!x2))
+			   | otherwise = toList $ update y1 Nothing $ fromList (a!!x1)
+		let f = \x -> case () of () | x == x1 -> b1 | x == x2 -> b2 |otherwise -> a!!x
+		let b = ([f 0] ++ [f 1] ++ [f 2] ++ [f 3] ++ [f 4] ++ [f 5] ++ [f 6] ++ [f 7])
+		b
 
+move::[String] -> Board -> IO()
+move x b = do
+		let board = change (read (x!!0)::Int) (read (x!!1)::Int) (read (x!!2)::Int) (read (x!!3)::Int) b 
+	  	let board' = (map.map) convert board
+	 	let print' n | n == 7    = print ((board'!!n))
+	 			  	 | otherwise = do 
+	 			  		print ((board'!!n))
+	 			  		print' (n+1) 
+	 	print' 0
+	 	c <- getLine
+	 	let x = (words $ c)
+	 	move x board
+
+convert::Maybe Piece -> String 
+convert (Just x) = show x 
+convert Nothing = "  "
+
+main = do
+	 c <- getLine
+	 let x = (words $ c)
+	 let board = change (read (x!!0)::Int) (read (x!!1)::Int) (read (x!!2)::Int) (read (x!!3)::Int) initialBoard
+
+	 let board' = (map.map) convert board
+	 let print' n | n == 7    = print ((board'!!n))
+	 			  | otherwise = do 
+	 			  	print ((board'!!n))
+	 			  	print' (n+1) 
+	 print' 0
+	 c <- getLine
+	 let x = (words $ c)
+	 move x board
+	 
